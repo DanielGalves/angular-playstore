c0f06ed955c3004848038d81cdf38d67feadfbbb
Daniel <danielgama1@live.com>
2023-09-25 23:55:17 -0300

    Att-6: Criando e estilizando o card Pricing e finalizando o projeto

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cards',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent {
+  @Input()
+  gameCover:string= ""
+  @Input()
+  gameLabel:string =""
+  @Input()
+  gameType:string=""
+  @Input()
+  gamePrice:string=""
 
 }
